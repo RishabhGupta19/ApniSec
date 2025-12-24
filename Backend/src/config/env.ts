086380dd380dbe292b5ba7765524796b794f0972
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-12-24 21:13:01 +0530

    Update env.ts

--- Backend/src/config/env.ts
+++ Backend/src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const ENV = {
   PORT: process.env.PORT || "5000",
   MONGO_URI: process.env.MONGO_URI as string,
-  JWT_SECRET: process.env.JWT_SECRET as string, // ✅ force string
+  JWT_SECRET: process.env.JWT_SECRET as string, 
   FRONTEND_URL: process.env.FRONTEND_URL as string,
   RESEND_API_KEY: process.env.RESEND_API_KEY as string,
    EMAIL_FROM: process.env.EMAIL_FROM as string,
